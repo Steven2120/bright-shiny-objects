5a29f67e04e9f703618bbe94bdc16646734ae5c5
Steven Moyano <stevenmoyano1@gmail.com>
2022-02-24 14:13:32 -0600

    completed tasks

--- main.js
+++ main.js
@@ -1,20 +1,70 @@
 // ┌─────────────────┐
 // │ Your code here! │
 // └─────────────────┘
+function createUser(first, last) {
+  let user = {
+    firstName: first,
+    lastName: last,
+  };
 
+  return user;
+}
 
+function setAge(user, age) {
+  user.age = age;
+  return user;
+}
 
+function incrementAge(user) {
+  user.age += 1;
+  return user;
+}
 
+function fixCar(car) {
+  car.needsMaintenance = false;
+  return car;
+}
 
+function addGrades(student, newGrades) {
+  for (let i = 0; i < newGrades.length; i++) {
+    student.grades.push(newGrades[i]);
+  }
+  return student;
+}
 
+function getDataType(obj, key) {
+  return typeof obj[key];
+}
 
+function addTodo(todos, newTodo) {
+  todos.push(newTodo);
+  return todos;
+}
 
+function addSong(playlist, newSong) {
+  playlist.duration = playlist.duration + newSong.duration;
+  playlist.songs = playlist.songs.concat(newSong);
+  return playlist;
+}
 
+function updateReportCard(reportCard, newGrade) {
+  let sum = 0;
+
+  reportCard.grades = reportCard.grades.concat(newGrade);
+
+  reportCard.lowestGrade = Math.min(...reportCard.grades);
+  reportCard.highestGrade = Math.max(...reportCard.grades);
+  for (let i = 0; i < reportCard.grades.length; i++) {
+    sum += reportCard.grades[i];
+  }
+  reportCard.averageGrade = sum / reportCard.grades.length;
+  return reportCard;
+}
 
 // ┌─────────────────────────────────────────────────────────────────────────┐
-// │ Code used for testing. Do not modify!                                   │                         
+// │ Code used for testing. Do not modify!                                   │
 // │                                                                         │
-const isDef = (arg) => arg === 'function';
+const isDef = (arg) => arg === "function";
 const pass = () => undefined;
 // │                                                                         │
 module.exports.createUser = isDef(typeof createUser) ? createUser : pass;
@@ -25,6 +75,8 @@ module.exports.addGrades = isDef(typeof addGrades) ? addGrades : pass;
 module.exports.getDataType = isDef(typeof getDataType) ? getDataType : pass;
 module.exports.addTodo = isDef(typeof addTodo) ? addTodo : pass;
 module.exports.addSong = isDef(typeof addSong) ? addSong : pass;
-module.exports.updateReportCard = isDef(typeof updateReportCard) ? updateReportCard : pass;
+module.exports.updateReportCard = isDef(typeof updateReportCard)
+  ? updateReportCard
+  : pass;
 // │                                                                         │
 // └─────────────────────────────────────────────────────────────────────────┘
